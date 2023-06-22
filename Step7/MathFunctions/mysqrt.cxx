fdc5ff5bd6a037c7bb6de76c2086768d6e0819ae
kryptoTomo <tomasz.madej101@gmail.com>
2023-06-22 13:59:13 +0200

    Step7

--- Step7/MathFunctions/mysqrt.cxx
+++ Step7/MathFunctions/mysqrt.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 
 // TODO 4: include cmath
+#include <cmath>
 #include "MathFunctions.h"
 
 // a hack square root calculation using simple operations
@@ -17,7 +18,11 @@ double mysqrt(double x)
   // else, use the existing logic.
 
   // Hint: Don't forget the #endif before returning the result!
-
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  double result = std::exp(std::log(x) * 0.5);
+  std::cout << "Computing sqrt of " << x << " to be " << result
+            << " using log and exp" << std::endl;
+#else
   double result = x;
 
   // do ten iterations
@@ -29,6 +34,7 @@ double mysqrt(double x)
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
+#endif
 
   return result;
 }
